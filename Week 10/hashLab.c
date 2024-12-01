29fe4c6833884b956b0d0a3cec6faa426361f297
Logan <ljordan@cocc.edu>
2024-11-30 20:16:30 -0800

    Implement hash table functions

--- Week 10/hashLab.c
+++ Week 10/hashLab.c
@@ -33,14 +33,18 @@ param1: word - the c-string to hash
 pre: str is not NULL
 return: the sum of the character values of the string are returned
 post: none
-HINT - you can treat chars like ints
-HINT - return the absolute value - negative hashes don't work
 */
 int hash(char *word) {
     printf("hashing %s \n", word);
-    /* FIX ME */
+    assert(word != NULL);
 
-    return 0;
+    int sum = 0;
+    int i;
+    for(i = 0; word[i] != '\0'; i++) {
+        sum += word[i];
+    }
+
+    return abs(sum);
 }
 
 /*
@@ -56,8 +60,17 @@ post: all hash links (buckets) are initialized to NULL
 */
 void initHash(struct hashTable *ht, int size) {
     printf("Initializing hashTable \n");
-    /* FIX ME */
+    assert(ht != NULL);
+    assert(size > 0);
 
+    ht->count = 0;
+    ht->cap = size;
+    ht->table = malloc(sizeof(struct hashLink*)*size);
+
+    int i;
+    for(i = 0; i < size; i++) {
+        ht->table[i] = NULL;
+    }
 }
 
 /*
@@ -67,20 +80,45 @@ param2: key - the key to insert
 param3: val - the value to insert
 pre: ht is not NULL
 post: hash table contains the key/value
-post: memory has been allocated dynamically for new hash link - if it is a
-new key - no duplicate keys are permitted
+post: memory has been allocated dynamically for new hash link - if it is a new key - no duplicate keys are permitted
 post: count has been incremented
 post: Resize table if load factor exceeds .75 - numHashLinks / capacity
-HINT: If the key already exists, then just update the value
-HINT: Add to the hashLink to the back of the linked list if it is a new key
-HINT: Use strcmp to compare c-strings
-HINT: integer division does not work as expected in c
 */
 void addHash(struct hashTable *ht, char *key, int val) {
     printf("Inserting: %s \n", key);
     assert(ht != NULL);
-    /* FIX ME */
 
+    if((double)ht->count / ht->cap > 0.75) {
+        reHash(ht, ht->cap*2);
+    }
+
+    int hashed = hash(key)%ht->cap;
+
+    /* Empty bucket */
+    if(ht->table[hashed] == NULL) {
+        struct hashLink *temp = malloc(sizeof(struct hashLink));
+        temp->key = key;
+        temp->val = val;
+        temp->next = NULL;
+        ht->table[hashed] = temp;
+        ht->count++;
+    } else {
+        /* Collision */
+        struct hashLink *curr = ht->table[hashed];
+        while(curr->next != NULL && strcmp(curr->key, key) != 0) {
+            curr = curr->next;
+        }
+        if(strcmp(curr->key, key) == 0) {
+            curr->val = val;
+        } else {
+            struct hashLink *temp = malloc(sizeof(struct hashLink));
+            temp->key = key;
+            temp->val = val;
+            temp->next = NULL;
+            curr->next = temp;
+            ht->count++;
+        }
+    }
 }
 
 /*
@@ -95,17 +133,32 @@ post: new hash table has cap size
 post: all keys have been re-hashed and inserted into new hash table
 post: all the old hash links have been freed
 post: previous array has been freed
-HINT - use addHash to re-hash the keys from old to new
 */
 void reHash(struct hashTable *ht, int size) {
-    printf("--------------------re-sizing table------------------------ \n");
+    printf("-------------------- re-sizing table ------------------------ \n");
+    assert(ht->count > 0);
+    assert(size > 0);
+
     struct hashLink **temp = ht->table;
     int tempSize = ht->cap;
     struct hashLink *curr, *del;
+
     ht->table = malloc(sizeof(struct hashLink*)*size);
     initHash(ht, size);
-    /* FIX ME */
 
+    int i;
+    for(i = 0; i < tempSize; i++) {
+        curr = temp[i];
+        while(curr != NULL) {
+            addHash(ht, curr->key, curr->val);
+            del = curr;
+            curr = curr->next;
+            free(del);
+            del = 0;
+        }
+    }
+    free(temp);
+    temp = 0;
 }
 
 /*
@@ -117,16 +170,31 @@ post: No changes to the hash table
 post: Map has been printed to standard output
 */
 void printHash(struct hashTable *ht) {
-    /* FIX ME */
+    printf("-------------------- printHash ------------------------ \n");
+    assert(ht != NULL);
 
+    struct hashLink *curr = NULL;
+    int i;
+    for(i = 0; i < ht->cap; i++) {
+        printf("Bucket %d: ", i);
+        curr = ht->table[i];
+        while(curr != NULL) {
+            printf("%s (%d), ", curr->key, curr->val);
+            curr = curr->next;
+        }
+        printf("\n");
+    }
 }
 
 
 int main() {
     printf("testing table... \n");
+
     struct hashTable *myTable;
     myTable = malloc(sizeof(struct hashTable));
+
     initHash(myTable, 10);
+
     addHash(myTable, "ken", 20);
     addHash(myTable, "dave", 24);
     addHash(myTable, "nora", 18);
@@ -137,6 +205,8 @@ int main() {
     addHash(myTable, "carl", 28);
     addHash(myTable, "carla", 36);
     addHash(myTable, "carlton", 17);
+
     printHash(myTable);
+
     return 0;
 }
